8bb3ccba9cb357206473acb1cb85e42ade11f920
Travis Brown <travjbrown98@gmail.com>
2019-10-24 14:36:26 +0000

    DB insertion not working

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # CSCD311 Class Project
 
-## Residency Web App by Travis Brown
+## Residency Web App by Travis Brown (10755131)
 
-### 10755131
+### Instructions
 
-### Lessons I may need to know for another time:
+- navigate to the root of this folder and run npm start in the console to start web application
+- on any browser, go to <http://localhost:3000>
+
+
+### Small things learned
 
 - Makes it so a file is accessible by everybody `sudo chmod -R go+w /file_path`
 
 - Allows me to kill a process at the specified port `lsof -nti:NumberOfPort | xargs kill -9`
+  
+- When console.logging Objects they print full contents if only the Obj but if so much as a string is in there with it. the Obj condenses to [object Object]
--- app.js
+++ app.js
@@ -2,35 +2,104 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
-var db = require('./databaseConfig');
+var alert = require('alert-node');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var registerRouter = require('./routes/register');
+//var alert = require('alert-node');
 
 var app = express();
+mongoose.connect('mongodb://localhost:27017/cscd311-class-project-api', 
+  {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useNewUrlParser: true
+  });
+var db = mongoose.connection;
+
+// Setting up the Schemas for the Database structure
+var Schema   = mongoose.Schema;
+const roomSchema = new Schema({
+  roomNum: Number,
+  occupants: [String],
+});
+const Room = mongoose.model('room', roomSchema);
+
+const studentSchema = new Schema({
+  id: String,
+  pin: String,
+  name: String,
+  level: String,
+  gender: String,
+});
+const Student = mongoose.model('Student', studentSchema);
+
+var currentUser = {
+  id:String,
+  pin:String,
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(function(req,res,next){
-  req.db = db;
-  next();
+/* GET home/login page. */
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Residency' });
 });
 
-app.use('/', indexRouter);
-app.use('/register', registerRouter);
+// POST saves login data in the submit form
+app.post('/', function(req,res,next){
+  if(req.body.id == null || req.body.pin == null){
+    // Send an alert message that something is empty
+    alert('Please fill out all fields');
+  }else{
+    currentUser.id = req.body.id;
+    currentUser.pin = req.body.pin;
+    res.render('register.ejs');
+  }
+  //sends us to this routing path established in app.js
+});
+
+// GET registration page
+app.get('/register', function(req, res) {
+  res.render('register');
+});
+
+/*POST Room registration. */
+app.post('/register', function(req, res){
+  console.log(req.body)
+  if(req.body.name == '' || req.body.level == '' || req.body.gender == '' || req.body.room == ''){
+        alert('Not all fields filled in!');
+  }else{
+      db.on('error', console.error.bind(console, 'connection error:'));
+      db.once('open', function() {
+        console.log("Connection Successful!");
+        var student = new Student(currentUser,req.body);
+
+        student.save().then(item => {
+          res.send("Saved to database");
+          res.render('showdb.ejs');
+        }).catch(err => {
+          res.status(400).send("Unable to save to database", err);
+        });
+      });
+    }
+});
 
 
 
+
+/// ERROR HANDLING
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
